d0ccdb053eadc725ce6ecf9b04aa10f3b7853541
hassy-haruto <tennsaihagekamisamanoiutouri@gmail.com>
2021-10-17 16:22:22 +0900

    varとletのスコープの違い

---
+++ JS/test.js
@@ -0,0 +1,17 @@
+//　osascript -l JavaScript sample.js
+
+const testScope = (scope) => {
+  if (scope === 'function') {
+    var functionScope = '関数スコープ内ならどこからでも参照できます'
+  }　else if (scope === 'block') {
+    let blockScope = 'ブロックスコープ内でしか参照できません'
+    console.log(blockScope)
+  }
+  console.log(functionScope)
+}
+
+testScope('function');
+
+// 実行↓
+// 関数スコープ内ならどこからでも参照できます
+
