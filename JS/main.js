12b9883deafe3b4ae3fbeeb7f3d9fa7721c2cfa1
hassy-haruto <tennsaihagekamisamanoiutouri@gmail.com>
2021-11-06 22:35:05 +0900

    vue2

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,15 @@
 (function() {
   'use strict';
+
+  // two way data vinding (to UI)がvueの特徴
+
+  // data vindingとは、dataとUIを結びつける意味
+  // two wayは、dataを更新すればUIが更新され、UIが更新されればdataが更新される
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      name: 'hashiguchi'
+    }
+  });
 })();
