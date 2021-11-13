f05c8e7a57fd50d89836afa3a85fdeb6a5c36cae
hassy-haruto <tennsaihagekamisamanoiutouri@gmail.com>
2021-11-13 21:50:28 +0900

    componentでいいね機能を使う

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,18 @@
 (function() {
   'use strict';
 
-// Components
-
   var likeComponent = Vue.extend({
-    template: '<button>Like</button>'
-    // template: '<button>Like</button>'
+    data: function() {
+      return {
+        count: 0
+      }
+    },
+    template: '<button @click="countUp">Like {{ count }}</button>',
+    methods: {
+      countUp: function() {
+        this.count++;
+      }
+    }
   });
 
 var app = new Vue({
