85a91540b497459e32d23103b4d2351b7993b60d
hassy-haruto <tennsaihagekamisamanoiutouri@gmail.com>
2021-11-13 21:42:18 +0900

    use component

--- JS/main.js
+++ JS/main.js
@@ -1,52 +1,19 @@
 (function() {
   'use strict';
 
-  // two way data vinding (to UI)がvueの特徴
+// Components
 
-  // data vindingとは、dataとUIを結びつける意味
-  // two wayは、dataを更新すればUIが更新され、UIが更新されればdataが更新される
-
-  var vm = new Vue({
-    el: '#app',
-    data: {
-      newItem: '',
-      todos: []
-    },
-    watch: {
-      todos: {
-        handler: function() {
-          localStorage.setItem('todos', JSON.stringify(this.todos));
-          // alert('Data saved!');
-        },
-        deep: true
-      }
-    },
-    mounted: function() {
-      this.todos - JSON.parse(localStorage.getItem('this.todos')) || [];
-    },
-    methods: {
-      addItem: function() {
-        var item = {
-          title: this.newItem,
-          isDone: false
-        };
-        this.todos.push(item);
-        this.newItem = '';
-      },
-      purge: function() {
-        if (!confirm('delete finished?')) {
-          return;
-        }
-          this.todos = this.remaining;
-      }
-    },
-
-computed: {
-  remaining: function() {
-    return this.todos.filter(function(todo) {
-      return !todo.isDone;
-    });
-  }
-}
+  var likeComponent = Vue.extend({
+    template: '<button>Like</button>'
+    // template: '<button>Like</button>'
   });
+
+var app = new Vue({
+  el: '#app',
+  components: {
+    'like-component' : likeComponent
+  }
+});
+
 })();
+
